f13dc3ee523e679e6a370dea823dcd6736397583
Abhi <meabhishek1512@gmail.com>
2024-04-21 17:01:56 +0530

    deployed

---
+++ client/src/redux/slices/apiSlice.js
@@ -0,0 +1,12 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react"
+
+// const API_URI = "http://localhost:8800/api"
+const API_URI = "https://taskzen-tm.onrender.com"
+
+const baseQuery = fetchBaseQuery({ baseUrl: API_URI + "/api" })
+
+export const apiSlice = createApi({
+    baseQuery,
+    tagTypes: [],
+    endpoints: (builder) => ({}),
+})
